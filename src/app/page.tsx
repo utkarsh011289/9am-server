ee4719c47c9a9cadfd43dee7c835219d710920f0
utkarsh011289 <ushukla218@gmail.com>
2024-08-13 22:50:46 +0530

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Register } from "@/Register/Register";
-
-export default function Home() {
-  return <div>
-            <Register />
-        </div>
-}
