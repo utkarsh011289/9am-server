5ea96f57157fc24120720a31c3a061ecb2cfe54c
utkarsh011289 <ushukla218@gmail.com>
2024-08-14 18:25:04 +0530

    latest

--- routes/students.js
+++ routes/students.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var mongodb=require('mongodb')
+var objectId= mongodb.ObjectId;
 var router = express.Router();
 var getDB=require('../common/dbConn')
 
@@ -24,6 +25,33 @@ router.get('/get-std',async function(req,res,next){
       }  catch(ex) {
             res.send(ex.message);
        }
+      })
 
-})
+      router.put('/update-std', async function(req,res,next){
+            try{
+                  const id=req.query.id;
+                  const data=req.body.data;
+                  const db= await getDB();
+                  const collection= db.collection("students")
+                  const result=await collection.updateOne( { _id: objectId.createFromHexString(id) },{ $set: data } )
+                  res.send(result)
+             }catch(ex){
+                   console.error(ex);
+                   res.send(ex.message);
+            }
+      })
+
+      router.delete('/delete-std/:id',async function(req,res,next){
+            try{
+                  var id=req.params.id
+                  const db=await getDB();
+                  const collection=db.collection("students")
+                  const result= await collection.deleteOne( { _id: objectId.createFromHexString(id) } )
+                  res.send(result)
+            }catch(ex){
+                  res.send(ex.message);
+
+            }
+                            
+      })
 module.exports = router ;
